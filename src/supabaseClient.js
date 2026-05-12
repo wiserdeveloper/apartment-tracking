fb1d14f8ca4b091161918b3f564aa4bccbaf38c7
Alyssa Wiser <104504122+wiserdeveloper@users.noreply.github.com>
2026-05-12 13:03:37 -0400

    Added env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://quqspfzcuicajvczkork.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1cXNwZnpjdWljYWp2Y3prb3JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MzQwNTMsImV4cCI6MjA5NDExMDA1M30.nIYElVTITEW36m7tEDF_itZQFvKtD68YmHfNf5_6ugs';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
